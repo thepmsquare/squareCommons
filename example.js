1c4a62a38bf5bd3747651cc32c43a57dc0c79248
Parth Mangtani <impmmmm@gmail.com>
2025-01-12 09:11:11 +0530

    📝 update example to represent real usage

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
 import { fetchJSONData } from "./dist/index.js";
-
-console.log(await fetchJSONData("http://localhost:10010", "", "GET"));
+try {
+  let output = await fetchJSONData("http://localhost:10010", "", "GET");
+  console.log(output);
+} catch (e) {
+  console.error("USER WILL SEE THIS ERROR MESSAGE: ", e.message);
+}
